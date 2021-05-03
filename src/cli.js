ae002def304a03f1c884c9552a033c5b81c3764a
Max Melkin <maxim.melkin@ya.ru>
2021-05-03 21:32:19 +0300

    updated cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,4 @@
 import readlineSync from 'readline-sync';
 
-console.log("Welcome to the Brain Games!")
-
-export const name = readlineSync.question('May I have your name? ');
+console.log("Welcome to the Brain Games!");
+export default let name = readlineSync.question('May I have your name? ');
