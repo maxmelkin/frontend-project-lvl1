6f67bc7e476c68428f6c0e9fb01c33c3fd299a86
Max Melkin <maxim.melkin@ya.ru>
2021-05-03 21:37:01 +0300

    lintered brain-games.js

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
 
 console.log("Welcome to the Brain Games!");
-export default let name = readlineSync.question('May I have your name? ');
+const userName = readlineSync.question('May I have your name? ');
+export default userName;
