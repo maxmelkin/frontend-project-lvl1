50b43105040e32ceb6fb1c3574e5ca2ee0680976
Max Melkin <maxim.melkin@ya.ru>
2021-05-05 11:31:36 +0300

    fixed import

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import workflow from '../src/index.js';
+import { workflow } from '../src/index.js';
 import { term, result } from '../games/game-brain-even.js';
 
 workflow(term, result);
