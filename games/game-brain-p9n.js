563985133002bd1237ea902d8c8788ed8f29baf9
Max Melkin <maxim.melkin@ya.ru>
2021-05-08 13:24:31 +0300

    cleaned up game-brain-p9n.js

--- games/game-brain-p9n.js
+++ games/game-brain-p9n.js
@@ -1,21 +1,11 @@
-import readlineSync from 'readline-sync';
-import { getRandomNum, getP9n } from '../src/index.js';
+// import readlineSync from 'readline-sync';
+import { getP9n } from '../src/index.js';
 
 console.log('Find the greatest common divisor of given numbers.');
 
 function game() {
   const p9n = getP9n();
   return p9n;
-
-//   const correctAnswer = String(getP9n(num1, num2));
-//   const userAnswer = readlineSync.question(`Question: ${num1} ${num2}\nYour answer: `);
-
-//   if (userAnswer === correctAnswer) {
-//     console.log('Correct!');
-//     return true;
-//   }
-//   console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
-//   return false;
 }
 
 export default game;
